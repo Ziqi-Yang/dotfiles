baf9e6ab0469d7045b589d343a9b079be63bfb8a
Zarkli Leonardo <mr.ziqiyang@gmail.com>
2022-09-27 19:03:20 +0800

    PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
 
 set PATH $PATH ~/myLocalBin/ $HOME/.cargo/bin ~/.npm-global/bin ~/go/bin
+set PATH $PATH ~/.local/bin
 
 set JAVA_HOME /usr/lib/jvm/default-java
 set _JAVA_OPTIONS

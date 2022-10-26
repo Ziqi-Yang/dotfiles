c1a59d8a80f9b895a4d12bf8a8d6192213954365
Zarkli Leonardo <mr.ziqiyang@gmail.com>
2022-10-26 18:48:29 +0800

    [fix + new] sync script and add new configs

--- .config/fish/functions/dtupdate.fish
+++ .config/fish/functions/dtupdate.fish
@@ -1,8 +1,10 @@
 function dtupdate
+  # use and to exit when error occurs
   cd ~/Documents/dotfiles
-  ./sync.sh
-  git add -A
-  git commit
-  git push
-  cd -
+  and ./sync.sh
+  and read -P "[press ENTER to continue]"
+  and git add -A
+  and git commit
+  and git push
+  and cd -
 end
--- sync.sh
+++ sync.sh
@@ -15,11 +15,14 @@ config_folders=("alacritty" "bspwm" "dunst" "fish" "neofetch"
 rofi_themes=("themes")
 
 # ----- files(root: home)
-files=(".xsession" ".config/rofimoji.rc")
+files=(".xsession" ".config/rofimoji.rc" ".Xresources" ".ideavimrc" ".todos.json" ".vimrc" ".gitconfig")
 
 
 # ============== sync ========================
+echo "[#] sync start"
+echo
 # ----- folders
+echo "[*] folders"
 for var in ${folder_vars[@]}; do 
   folders=$var[@]
   if [[ $var == "config_folders" ]]; then
@@ -37,14 +40,23 @@ for var in ${folder_vars[@]}; do
     mkdir -p $local_path
     fullpath="$prefix/$f2"
     cp $fullpath $local_path -rf
+    echo "$fullpath --- done"
   done
 done
 
+echo
+echo "--------------------------"
+echo
 # ----- files
+echo "[*] files"
 for f in ${files[@]}; do
   fullpath="$HOME/$f"
   local_folder="$local_repo/$(dirname $f)"
   local_full_path="$local_repo/$f"
   mkdir -p $local_folder
   cp $fullpath $local_full_path -f
+  echo "$fullpath --- done"
 done
+
+echo
+echo "[#] sync done"

0c01cd5da0e13cdd3ea61580ac10aee668ad4d2a
Zarkli Leonardo <mr.ziqiyang@gmail.com>
2022-10-26 18:33:58 +0800

    [feature] add sync script to sync config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,18 @@
 if status is-interactive
 
 # environment variables
+
+## other
+set EDITOR "nvim"
+set ANDROID_SDK_ROOT /opt/android-sdk
+
 ## path
 set PATH $PATH ~/myBin/ $HOME/.cargo/bin ~/.npm-global/bin ~/go/bin
 set PATH $PATH ~/.local/bin ~/fvm/default/bin
+set PATH $PATH $ANDROID_SDK_ROOT/platform-tools/
+set PATH $PATH $ANDROID_SDK_ROOT/tools/bin/
+set PATH $PATH $ANDROID_ROOT/emulator
+set PATH $PATH $ANDROID_SDK_ROOT/tools/
 
 ## java
 set JAVA_HOME /usr/lib/jvm/default-java
@@ -13,8 +22,7 @@ set _JAVA_OPTIONS
 set FLUTTER_STORAGE_BASE_URL "https://mirrors.tuna.tsinghua.edu.cn/flutter"
 set PUB_HOSTED_URL "https://mirrors.tuna.tsinghua.edu.cn/dart-pub"
 
-## other
-set EDITOR "nvim"
+
 
 end
 
--- .config/fish/functions/doom.fish
+++ .config/fish/functions/doom.fish
@@ -1,8 +1,9 @@
 function doom
+    # set git proxy is the best way
+    # ~/.emacs.d/bin/doom $argv
+    
     # try both if error occured
     # proxychains ~/.emacs.d/bin/doom $argv
-    # env all_proxy=http://127.0.0.1:7890 ~/.emacs.d/bin/doom $argv
+    env all_proxy=http://127.0.0.1:7890 ~/.emacs.d/bin/doom $argv
     
-    # set git proxy is the best way
-    ~/.emacs.d/bin/doom $argv
 end

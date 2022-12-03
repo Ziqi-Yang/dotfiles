36a4ac875011ea822e0787560fc7493b163b48d5
Zarkli Leonardo <mr.ziqiyang@gmail.com>
2022-12-03 16:47:38 +0800

    new:polybar pomodoro change

---
+++ dotfiles/config/polybar/scripts/pomodoro.sh
@@ -0,0 +1,29 @@
+#!/bin/env bash
+# only two session named "work" and "rest" in uair.toml
+
+work_count=1
+rest_count=1
+loop_num=4
+
+echo "%{F#7f8c8d}%{F-}"
+while true
+do
+    while read -r line; do
+        read -a line <<< "$line"
+        if [[ "${line[0]}" == "work" ]]; then
+            echo "%{F#ff5770} %{F#2980b9}[${work_count}/${loop_num}] %{F#f39c12}${line[0]} %{F#27ae60}${line[1]}%{F-}"
+            if [[ "${line[1]}" == "0s" ]]; then
+                work_count=$(( "$work_count" % "$loop_num" + 1 ))
+            fi
+            echo $work_count
+        elif [[ "${line[0]}" == "rest" ]]; then
+            echo "%{F#7f8c8d} %{F#2980b9}[${rest_count}/${loop_num}] %{F#f39c12}${line[0]} %{F#27ae60}${line[1]}%{F-}"
+            if [[ "${line[1]}" == "0s" ]]; then
+                rest_count=$(( "$rest_count" % "$loop_num" + 1 ))
+            fi
+            echo $rest_count
+        else
+            echo Error!
+        fi
+    done < <(uair)
+done

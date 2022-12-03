58fb60d6f7833986adb74f19b4bfb85a7a6319ed
Zarkli Leonardo <mr.ziqiyang@gmail.com>
2022-12-03 21:05:32 +0800

    pitch:bspwm add emacs scratch frame external rule

--- dotfiles/config/bspwm/external_rules.sh
+++ dotfiles/config/bspwm/external_rules.sh
@@ -7,4 +7,8 @@ consequences="$4"
 title="$(xdotool getwindowname "$wid")"
 
 # emacs-everywhere
-[ "$title" = "emacs-everywhere" ] && echo 'state=floating rectangle=1600x1000+800+500'
+if [[  "$title" = "emacs-everywhere"  ]]; then
+    echo 'state=floating rectangle=1600x1000+800+500'
+elif [[  "$title" = "scratchemacs-frame"  ]]; then
+    echo 'state=floating rectangle=1600x1000+800+500'
+fi

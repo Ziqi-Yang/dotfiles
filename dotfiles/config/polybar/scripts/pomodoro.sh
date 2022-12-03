cbf212334c232c46cdf8ddca473941c5fa0e497a
Zarkli Leonardo <mr.ziqiyang@gmail.com>
2022-12-03 17:04:03 +0800

    pitch:polybar pomodoro color change

--- dotfiles/config/polybar/scripts/pomodoro.sh
+++ dotfiles/config/polybar/scripts/pomodoro.sh
@@ -11,13 +11,13 @@ do
     while read -r line; do
         read -a line <<< "$line"
         if [[ "${line[0]}" == "work" ]]; then
-            echo "%{F#ff5770} %{F#2980b9}[${work_count}/${loop_num}] %{F#f39c12}${line[0]} %{F#27ae60}${line[1]}%{F-}"
+            echo "%{F#ff5770} %{F-}[${work_count}/${loop_num}] %{F#f39c12}${line[0]} %{F#27ae60}${line[1]}%{F-}"
             if [[ "${line[1]}" == "0s" ]]; then
                 work_count=$(( "$work_count" % "$loop_num" + 1 ))
             fi
             echo $work_count
         elif [[ "${line[0]}" == "rest" ]]; then
-            echo "%{F#7f8c8d} %{F#2980b9}[${rest_count}/${loop_num}] %{F#f39c12}${line[0]} %{F#27ae60}${line[1]}%{F-}"
+            echo "%{F#7f8c8d} %{F-}[${rest_count}/${loop_num}] %{F#f39c12}${line[0]} %{F#27ae60}${line[1]}%{F-}"
             if [[ "${line[1]}" == "0s" ]]; then
                 rest_count=$(( "$rest_count" % "$loop_num" + 1 ))
             fi

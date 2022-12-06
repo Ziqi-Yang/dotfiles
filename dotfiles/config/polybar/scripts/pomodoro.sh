bb1d0ed4f477872cf0fe115aabf26f7f4fb31012
Zarkli Leonardo <mr.ziqiyang@gmail.com>
2022-12-06 10:50:59 +0800

    fix:uair add loop support

--- dotfiles/config/polybar/scripts/pomodoro.sh
+++ dotfiles/config/polybar/scripts/pomodoro.sh
@@ -6,24 +6,24 @@ rest_count=1
 loop_num=4
 
 echo "%{F#7f8c8d}%{F-}"
-while true
-do
-    while read -r line; do
-        read -a line <<< "$line"
-        if [[ "${line[0]}" == "work" ]]; then
-            echo "%{F#ff5770} %{F-}[${work_count}/${loop_num}] %{F#f39c12}${line[0]} %{F#27ae60}${line[1]}%{F-}"
-            if [[ "${line[1]}" == "0s" ]]; then
-                work_count=$(( "$work_count" % "$loop_num" + 1 ))
-            fi
-            echo $work_count
-        elif [[ "${line[0]}" == "rest" ]]; then
-            echo "%{F#7f8c8d} %{F-}[${rest_count}/${loop_num}] %{F#f39c12}${line[0]} %{F#27ae60}${line[1]}%{F-}"
-            if [[ "${line[1]}" == "0s" ]]; then
-                rest_count=$(( "$rest_count" % "$loop_num" + 1 ))
-            fi
-            echo $rest_count
-        else
-            echo Error!
+# while true
+# do
+while read -r line; do
+    read -a line <<< "$line"
+    if [[ "${line[0]}" == "work" ]]; then
+        echo "%{F#ff5770} %{F-}[${work_count}/${loop_num}] %{F#f39c12}${line[0]} %{F#27ae60}${line[1]}%{F-}"
+        if [[ "${line[1]}" == "0s" ]]; then
+            work_count=$(( "$work_count" % "$loop_num" + 1 ))
         fi
-    done < <(uair)
-done
+        echo $work_count
+    elif [[ "${line[0]}" == "rest" ]]; then
+        echo "%{F#7f8c8d} %{F-}[${rest_count}/${loop_num}] %{F#f39c12}${line[0]} %{F#27ae60}${line[1]}%{F-}"
+        if [[ "${line[1]}" == "0s" ]]; then
+            rest_count=$(( "$rest_count" % "$loop_num" + 1 ))
+        fi
+        echo $rest_count
+    else
+        echo Error!
+    fi
+done < <(uair)
+# done

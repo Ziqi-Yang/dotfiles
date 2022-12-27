2bc7b601e3343d0a72337b822c1d041056a7f378
Zarkli Leonardo <mr.ziqiyang@gmail.com>
2022-12-27 11:52:23 +0800

    feature:fish use fisher plugin manager change sioyek keybindings

--- dotfiles/config/fish/functions/t.fish
+++ dotfiles/config/fish/functions/t.fish
@@ -1,6 +1,6 @@
 function t
   set -l word $argv[1]
   # ydict -c -v 1 $argv
-  wd $word
   wget -q "https://dict.youdao.com/dictvoice?audio=$word&type=2" -O /tmp/t_speak.mp3 && nohup mpg123 -q /tmp/t_speak.mp3 > /dev/null &
+  wd $word | less -R
 end

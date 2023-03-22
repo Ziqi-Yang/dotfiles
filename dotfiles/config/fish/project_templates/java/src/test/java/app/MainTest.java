cf19721f7b5da21b84960c791b6b77f0a3d4a15c
Zarkli Leonardo <mr.ziqiyang@gmail.com>
2023-03-22 21:56:03 +0800

    feat(java): java project template

--- dotfiles/config/fish/project_templates/java/src/test/java/app/MainTest.java
+++ dotfiles/config/fish/project_templates/java/src/test/java/app/MainTest.java
@@ -1,11 +1,12 @@
+package app;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MyTests {
+class MainTest {
     @Test
     void simpleTest() {
         int expected = 2 + 3;
-        int actual = 6;
+        int actual = 5;
         assertEquals(expected, actual, "2 + 3 should equal 5");
     }
 }

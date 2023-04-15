c66f946484780ad31b9b9e448f43b36f684767da
Zarkli Leonardo <mr.ziqiyang@gmail.com>
2023-04-15 17:09:13 +0800

    update

--- dotfiles/config/fish/functions/pinit/cc.fish
+++ dotfiles/config/fish/functions/pinit/cc.fish
@@ -2,5 +2,4 @@ function pinit_cc -a 'packageName'
   set template "$HOME/.config/fish/project_templates/cc"
   cp -rT $template "$packageName" # use -T to also copy hidden files
   sed -i "s/#{packageName}#/$packageName/g" ./$packageName/README.org
-  go mod init github.com/Ziqi-Yang/$packageName
 end

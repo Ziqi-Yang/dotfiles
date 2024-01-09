ec77aaa35f0db3b0eecc1e24d2c272fabb511464
Meow King <mr.meowking@anche.no>
2024-01-09 22:28:18 +0800

    update

--- dotfiles/config/fish/functions/,cht.fish
+++ dotfiles/config/fish/functions/,cht.fish
@@ -1,4 +1,4 @@
-function cht
+function ,cht
   env all_proxy=http://127.0.0.1:7890 cht.sh $argv | less -iR
 end
 
--- dotfiles/config/sway/autostart.sh
+++ dotfiles/config/sway/autostart.sh
@@ -18,6 +18,10 @@ foot --server &
 telegram-desktop &
 firefox-developer-edition &
 
+# enter gpg password (used for gpg agent), so offlineimap sync in crontab can
+# work correctly
+sleep 3 && pass nonsense_password &
+
 # not-working apps
 # nm-applet & # cannot stay within waybar tray
 # megasync & # cannot work on wayland
